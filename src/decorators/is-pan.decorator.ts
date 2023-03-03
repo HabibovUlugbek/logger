a317c25bede5426f43afe10bceeb4230faf1a900
Sukhrob <iaxel0297@gmail.com>
2023-03-03 15:38:38 +0500

    Update IsPan decorator

--- src/decorators/is-pan.decorator.ts
+++ src/decorators/is-pan.decorator.ts
@@ -1,15 +1,15 @@
-import { ValidateBy, buildMessage } from 'class-validator'
 import type { ValidationOptions } from 'class-validator'
+import { ValidateBy, buildMessage } from 'class-validator'
 import { isPan } from '@validators'
 
-export const IsPan = (options?: ValidationOptions): PropertyDecorator =>
+export const IsPan = (validationOptions?: ValidationOptions): PropertyDecorator =>
   ValidateBy({
     name: 'IsPan',
     validator: {
       validate: (value: unknown): boolean => isPan(value),
       defaultMessage: buildMessage(
         (each: string): string => each + 'The value of $property has the incorrect format.',
-        options,
+        validationOptions,
       ),
     },
   })
