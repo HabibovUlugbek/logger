0302ac496d93ea2ea6c44ef9feb87ce6a845ae5e
Sukhrob <iaxel0297@gmail.com>
2023-03-03 15:39:00 +0500

    Update isCode validator

--- src/validators/is-code.validator.ts
+++ src/validators/is-code.validator.ts
@@ -1,11 +1,11 @@
 import type { IsCodeOptions } from '@interfaces'
 
 export const isCode = (value: unknown, options?: IsCodeOptions): boolean => {
-  if (options && options.type === 'string') {
+  if (options?.type === 'string') {
     return typeof value === 'string' && value.length === options.length && /^[A-Za-z0-9]+$/.test(value)
   }
 
-  if (options && options.type === 'number') {
+  if (options?.type === 'number') {
     return typeof value === 'number' && value.toString().length === options.length && /^[0-9]+$/.test(value.toString())
   }
 
