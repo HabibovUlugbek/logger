d1fd1ad110fc7a225a1dd9cc80fe0847b774db71
HabibovUlugbek <habibovulugbek22@gmail.com>
2023-03-28 18:00:58 +0500

    added logging error

--- src/middlewares/rpc-logger.ts
+++ src/middlewares/rpc-logger.ts
@@ -1,10 +1,10 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common'
 import { Observable } from 'rxjs'
-import { tap } from 'rxjs/operators'
+import { catchError, tap } from 'rxjs/operators'
 
 @Injectable()
 export class RpcLoggingInterceptor implements NestInterceptor {
-  private logger: Logger = new Logger('CommandLogger')
+  private logger: Logger = new Logger('Logger')
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const now = Date.now()
@@ -14,14 +14,29 @@ export class RpcLoggingInterceptor implements NestInterceptor {
       tap((data) => {
         const elapsed = Date.now() - now
         const logData = {
-          command: pattern,
           payload: payload[0],
+          command: pattern,
           response: data,
           begin_date: new Date(now).toISOString(),
           end_date: new Date().toISOString(),
           elapsed_time: `${elapsed}ms`,
         }
         this.logger.log(JSON.stringify(logData))
+      }),
+      catchError((error) => {
+        const elapsed = Date.now() - now
+        const logData = {
+          payload: payload[0],
+          command: pattern,
+          error: error.message,
+          begin_date: new Date(now).toISOString(),
+          end_date: new Date().toISOString(),
+          elapsed_time: `${elapsed}ms`,
+        }
+
+        console.log(logData)
+
+        throw error
       }),
     )
   }
