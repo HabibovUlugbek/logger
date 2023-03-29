30c05fe7cf03e04aa2edac44d31001de16e5673d
HabibovUlugbek <habibovulugbek22@gmail.com>
2023-03-29 11:07:42 +0500

    httpinterceptor

--- src/middlewares/http-logger.ts
+++ src/middlewares/http-logger.ts
@@ -1,21 +1,16 @@
-import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-} from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { catchError, tap } from 'rxjs/operators'
 
 @Injectable()
 export class HttpLoggerInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.switchToHttp().getRequest();
-    const { method, originalUrl, body } = request;
+    const request = context.switchToHttp().getRequest()
+    const { method, originalUrl, body } = request
     return next.handle().pipe(
       tap((data) => {
-        const { statusCode } = context.switchToHttp().getResponse();
-        const elapsedTime = Date.now() - request.startAt;
+        const { statusCode } = context.switchToHttp().getResponse()
+        const elapsedTime = Date.now() - request.startAt
         const log = {
           method,
           originalUrl,
@@ -23,21 +18,21 @@ export class HttpLoggerInterceptor implements NestInterceptor {
           statusCode,
           res: data,
           elapsedTime,
-        };
-        console.log(log);
+        }
+        console.log(log)
       }),
       catchError((error) => {
-        const elapsedTime = Date.now() - request.startAt;
+        const elapsedTime = Date.now() - request.startAt
         const log = {
           method,
           originalUrl,
           body,
           res: error.message,
           elapsedTime,
-        };
-        console.log(log);
-        throw error;
+        }
+        console.log(log)
+        throw error
       }),
-    );
+    )
   }
 }
